30d3fed88d7f91b67c543db19677f5ea85036461
Arnold Haban <hsbyte@gmail.com>
2020-05-14 16:21:50 -0400

    code update

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  htmlWhitespaceSensitivity: 'css',
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  endOfLine: 'auto',
+  semi: true,
+  arrowParens: 'avoid',
+};
---
+++ src/models/EventModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const eventSchema = new mongoose.Schema({
+    event: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    }
+}, { timestamps: true } );
